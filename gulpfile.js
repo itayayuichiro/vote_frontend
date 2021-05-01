5f46fecfebb7c11d20e4582a909a9bcf23ae5c7a
村上大地 <micolumn@MacBook-Pro.local>
2021-05-01 09:24:42 +0900

    開発環境の追加、loginページのサンプルイメージ作成

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,61 @@
-var gulp         = require('gulp');
-var plumber      = require("gulp-plumber");
-var sass         = require('gulp-sass');
+'use strict';
 
-var paths = {
-  'css'    : 'css/'
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require("gulp-rename");
+var ejs = require("gulp-ejs");
+var browserSync = require('browser-sync');
+
+var config = {
+  server: {
+    open: 'external',
+    port: 8000,
+    server: {
+      baseDir: './app/',
+      index: 'index.html'
+    },
+    notify: false,
+    ghostMode: false
+  }
 }
 
-//Sass
-var sassOptions = {
-  outputStyle: 'compressed'//圧縮設定 nested, expanded, compact, compressed
-}
-gulp.task('sass', function (done) {
-  gulp.src('scss/*.scss')
-    .pipe(sass(sassOptions))
-    .pipe(gulp.dest(paths.css));
+//server
+gulp.task('server', (done) => {
+  browserSync(config.server);
   done();
+});
+
+//sassのコンパイル
+var sassOptions = {
+  outputStyle: 'nested'//圧縮設定 nested, expanded, compact, compressed
+}
+gulp.task('sass', (done) => {
+  gulp
+    .src('./app/scss/*.scss')
+    .pipe(sass(sassOptions))
+    .pipe(gulp.dest('./app/css/'));
+  done();
+});
+
+//ejsのコンパイル
+gulp.task('ejs', (done) => {
+  gulp
+    .src(['./app/ejs/**/*.ejs', '!' + 'ejs/**/_*.ejs'])
+    .pipe(ejs({}, {}, {ext:'.html'}))
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest('./app/'));
+  done();
+});
+
+//リロード
+gulp.task('browser-reload', (done) => {
+  browserSync.reload();
+  done();
+});
+
+//watch
+gulp.task('watch', () => {
+  browserSync(config.server);
+  gulp.watch('./app/scss/**/*.scss', gulp.series(['sass','browser-reload']));
+  gulp.watch('./app/ejs/**/*.ejs', gulp.series(['ejs','browser-reload']));
 });
